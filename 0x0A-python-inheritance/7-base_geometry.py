03fb84c03d6e661e784d98cfade0aa9a70d4262d
SaronTeb <saripari61@gmail.com>
2022-08-01 11:10:57 -0700

    0A

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
 """
-Contains definition of the class BaseGeometry
+Contains the class BaseGeometry
 """
 
 
-class BaseGeometry():
-        """Definition of class BaseGeometry"""
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
 
-            def area(self):
-                        """Definition of area method.
-           Raises an Exception with message 'area() is not implemented'"""
-                                raise Exception("area() is not implemented")
-
-                                def integer_validator(self, name, value):
-                                            """Validates value to be an int grater than 0.
-        Args:
-            name (str): name of the value
-            value (unknown): to be validated.
-        Raises:
-            TypeError: if value is not an int.
-            ValueError: if value is <= 0.
-        """
-                                                    if type(value) != int:
-                                                                    raise TypeError("{} must be an integer".format(name))
-                                                                        if value <= 0:
-                                                                                        raise ValueError("{} must be greater than 0".format(name))
-                                                                                    
+    def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
